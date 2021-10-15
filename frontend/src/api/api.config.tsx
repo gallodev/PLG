f0f5d4c96092b96df8bc583729fdf34dfba1c1a7
Christian Gallo <contato.gallodev@gmail.com>
2021-10-15 16:12:45 -0300

    fix(Console): remove ununsable console log

--- frontend/src/api/api.config.tsx
+++ frontend/src/api/api.config.tsx
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-export const getBaseURL = (baseResource ?: string) => {  
-    
+export const getBaseURL = (baseResource ?: string) => {      
     const { REACT_APP_HEROKU_API } = process.env;
-    console.log(REACT_APP_HEROKU_API)
+    
     const getUrl = REACT_APP_HEROKU_API || 'http://localhost:3333';
     const apiPath = (baseResource) ? `/api/${baseResource}`:'/api';    
     const url = `${getUrl}${apiPath}`;  
