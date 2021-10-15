7fabb6b561d1046abddf09e6033465f36aa42ba3
Christian Gallo <contato.gallodev@gmail.com>
2021-10-14 23:34:39 -0300

    feat(source): update

--- frontend/src/api/api.config.tsx
+++ frontend/src/api/api.config.tsx
@@ -2,10 +2,11 @@ import axios from 'axios';
 
 export const getBaseURL = (baseResource ?: string) => {  
     
-    const { HEROKU_API } = process.env;
-    const getUrl = HEROKU_API || 'localhost:3333';
+    const { REACT_APP_HEROKU_API } = process.env;
+    console.log(REACT_APP_HEROKU_API)
+    const getUrl = REACT_APP_HEROKU_API || 'http://localhost:3333';
     const apiPath = (baseResource) ? `/api/${baseResource}`:'/api';    
-    const url = `http://${getUrl}${apiPath}`;  
+    const url = `${getUrl}${apiPath}`;  
   
     return url;
 };
